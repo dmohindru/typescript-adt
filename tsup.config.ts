bdc40a58895ba4770ff48e803fe2ecb23a4d778b
Dhruv Mohindru <dmohindru@gmail.com>
2024-08-05 20:01:00 +1000

    Got a working setup for compatible with other typescript projects

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,14 +2,15 @@ import { defineConfig } from 'tsup';
 
 export default defineConfig({
   format: ['cjs', 'esm'],
+  target: ['es2022', 'node18'],
   entry: {
     index: 'src/index.ts',
     'list/index': 'src/list/index.ts',
     'interfaces/index': 'src/interfaces/index.ts',
   },
-  dts: true,
-  shims: true,
-  skipNodeModulesBundle: true,
   clean: true,
-  sourcemap: true,
+  dts: false,
+  minify: true,
+  sourcemap: false,
+  splitting: true,
 });
